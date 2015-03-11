3aaa07a962b26425253d4e3cab7907ab38027337
Léo Berbare <leo@berbare.com.br>
2015-03-10 21:43:49 -0300

    Create TaskType model (scaffold)

---
+++ db/migrate/20150311003846_create_task_types.rb
@@ -0,0 +1,12 @@
+class CreateTaskTypes < ActiveRecord::Migration
+  def change
+    create_table :task_types do |t|
+      t.string :title
+      t.string :week_days
+      t.int :each_n_weeks
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
