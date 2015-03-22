0f86be10ed79ee0e160a5179ab8dd371b44fc66b
Léo Berbare <leo@berbare.com.br>
2015-03-22 19:08:52 -0300

    New homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+  
+  # GET /
   def index
+  	@entity_classes = [
+  		[:employees, 'user'],
+  		[:employee_types, 'users'],
+  		[:places, 'bed'],
+  		[:place_types, 'home'],
+  		[:task_domains, 'puzzle-piece'],
+  		[:task_types, 'tasks'],
+  		[:tasks, 'clock-o'],
+  		[:tools, 'wrench']
+  	]
   end
+
 end
