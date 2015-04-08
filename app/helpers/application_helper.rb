1677cb7416784f8548f759f2d8f51b995c598999
Léo Berbare <leo@berbare.com.br>
2015-04-08 12:13:21 -0300

    Locale dates and Task summary (view component)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
 		[:employees, :employee_types, :places, :place_types, :responsibilities, :task_domains, :task_types, :tasks, :tools]
 	end
 
+  def cool_date dt
+    unless dt.nil?
+      l(dt).downcase
+    end
+  end
+
+  def cool_date_interval dt1, dt2
+    unless dt1.nil? || dt2.nil?
+      str = (l dt1) << " - "
+      if dt1.day != dt2.day
+        str << (l dt2)
+      else
+        str << (l dt2, format: :short)
+      end
+    end
+  end
+
 end
